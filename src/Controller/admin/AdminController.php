f4fc99f5f58f1b26b4e3a0c6635c9642620e8265
Andreotti Ludovic <andr.ludow@gmail.com>
2023-02-13 15:19:51 +0100

    Add categories add/delete

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -6,19 +6,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 #[route('/admin', name:'app_admin_')]
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function admin(): Response
     {
-
-        
-
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
+  
 
 
 }
---
+++ src/Controller/admin/CategoryController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Entity\Categories;
+use App\Form\CategoryType;
+use App\Repository\CategoriesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/admin/categories', name: 'app_admin_category_')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'add')]
+    public function category(EntityManagerInterface $em,Request $request,CategoriesRepository $categoriesRepository): Response
+    {
+        $category = new Categories();
+        $formCategories = $this->createForm(CategoryType::class, $category);
+        
+        $formCategories -> handleRequest($request);
+        if($formCategories-> isSubmitted() && $formCategories-> isValid() )
+        {
+            $category = $formCategories ->getData();
+            $em -> persist($category);
+            $em -> flush();
+
+            $this -> addFlash('succes','c\'est enregistré');
+
+            return $this-> redirectToRoute('app_admin_category_add');
+        }
+
+        $categories = $categoriesRepository->findAll();
+
+
+        return $this->render('admin/category.html.twig', [
+            'form' => $formCategories ->createView(),
+            'categories' => $categories
+        ]);
+    }
+
+    #[Route('/categories/{id}/supprimer',name:'delete')]
+    public function DeleteCategory(EntityManagerInterface $em, Categories $category)
+    {
+        $em -> remove($category);
+        $em -> flush();
+
+         return $this->redirectToRoute('app_admin_category_add');
+    }
+}
